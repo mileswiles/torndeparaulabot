16189a15ff13f45b59f03d66232576afe9475318
mileswiles <nilsn_1416@hotmail.com>
2019-11-13 11:53:46 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,4 @@
+import telegram
+bot = telegram.Bot(token='927270269:AAEMr5W9cgYjmFP7OMvhBwYojf7C7VT8xGo')
+print(bot.get_me())
+{"first_name": "Toledo's Palace Bot", "username": "ToledosPalaceBot"}
